b3597004ff32fa7388fae584e482545366ec1318
Andlepiha <rutsov@ea.spbstu.ru>
2023-04-08 21:03:30 +0300

    Module 5

---
+++ js/util.js
@@ -0,0 +1,13 @@
+function getRandom(from, to) {
+  if (from > to) {
+    return -1;
+  }
+
+  from = Math.ceil(from);
+  to = Math.floor(to);
+  return Math.floor(Math.random() * (to - from + 1)) + from;
+}
+
+function checkMaxLength(string, maxLength) {
+  return string.length <= maxLength;
+}
